4fc2a934e26377979c503d1952b7272f41211d7c
hulingzhi <hulingzhi@cvte.cn>
2014-06-10 08:26:22 +0800

    Initial Commit

---
+++ Demo/FDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FDAppDelegate.h
+//  Demo
+//
+//  Created by hulingzhi on 14-6-10.
+//  Copyright (c) 2014年 hulingzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by hulingzhi on 14-6-10.
+//  Copyright (c) 2014年 hulingzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FDAppDelegate class]));
+    }
+}
